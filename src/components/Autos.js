c8be1a27b37c122e627e1274d03709ec3b76a3a1
JasonHerrera <jasonjosebaca@gmail.com>
2023-10-02 00:20:02 -0600

    detallitos a el anterior commit

--- src/components/Autos.js
+++ src/components/Autos.js
@@ -1,12 +1,14 @@
+import './styles.css'
+
 function Autos_card(props) {
     return (
-        <div className="col-md-3">
-            <div class="card text-dark" style={{ width: '18' + 'rem' }}>
+        <div className="col-md-3 cardst">
+            <div class="card text-dark" style={{ width: '18rem'}}>
                 <img src={props.Poster} class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title">{props.Name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">De: {props.Origin}</h6>
-                    <p class="card-text">{props.Description}</p>
+                    <p class="card-text">{limitDescription(props.Description, 100)}</p>
                     <div class="card-footer">
                         {props.Year}
                     </div>
@@ -15,6 +17,13 @@ function Autos_card(props) {
         </div>
 
     );
+}
+
+function limitDescription(description, maxLength) {
+    if (description.length > maxLength) {
+        return description.slice(0, maxLength) + "...";
+    }
+    return description;
 }
 
 export default Autos_card;
