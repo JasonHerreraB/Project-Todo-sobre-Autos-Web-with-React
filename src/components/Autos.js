6be0d4b32f60f59f3baecd03fd5fa9fde275d7af
JasonHerrera <jasonjosebaca@gmail.com>
2023-10-02 23:05:38 -0600

    Estilo sutilmente mejorado y readme moded

--- src/components/Autos.js
+++ src/components/Autos.js
@@ -3,9 +3,9 @@ import './styles.css'
 function Autos_card(props) {
     return (
         <div className="col-md-3 cardst">
-            <div class="card text-dark" style={{ width: '18rem'}}>
+            <div class="card text-dark  navstile" style={{ width: '18rem'}}>
                 <img src={props.Poster} class="card-img-top" alt="..." />
-                <div class="card-body">
+                <div class="card-body ">
                     <h5 class="card-title">{props.Name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">De: {props.Origin}</h6>
                     <p class="card-text">{limitDescription(props.Description, 100)}</p>
--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -23,7 +23,7 @@ function Navegacion() {
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Opciones
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <ul class="dropdown-menu  navstile" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Action</a></li>
                                 <li><a class="dropdown-item" href="#">Another action</a></li>
                                 <li><hr class="dropdown-divider" /></li>
